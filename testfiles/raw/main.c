fcffb1fc3e5274903ef1462a778a6d8a1baf2927
sabi1k98 <rsabol1k98@gmail.com>
2019-08-09 19:14:27 +0200

    [hw3] added test assembly

---
+++ testfiles/raw/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern int npown(int base, int exp);
+
+int main(void) {
+    printf("%d\n", npown(5, 3));
+    return 0;
+}
---
+++ testfiles/raw/program.s
@@ -0,0 +1,20 @@
+.GLOBAL npown
+npown:
+    mov %rsi, %rax
+    mov %rdi, %rbx
+    cmp $0, %rax
+    je one
+    jmp loop
+mul:
+    imul %rbx, %rdi
+loop:
+    add $-1, %rax
+    cmp $0, %rax
+    jne mul
+    jmp end
+one:
+    add $1, %rax
+    ret
+end:
+    mov %rdi, %rax
+    ret
